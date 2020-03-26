3936874cee6eb11becb36c2fefa0540890959620
Adam Mills <almills@unomaha.edu>
2020-03-26 15:39:42 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -10,24 +10,47 @@ console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
 */
 
-let cards = ["queen","queen","king","king"];
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+}
+];
+
+
 let cardsInPlay = [];
 
 function checkForMatch(){
 if(cardsInPlay.length == 2)	{
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 }
 }
 
 function flipCard(cardId){
-
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
-
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 checkForMatch();
 }
 
