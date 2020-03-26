91964cb00dcba7b649c5cdd71e8eb925d9e8d9b5
Adam Mills <almills@unomaha.edu>
2020-03-26 12:23:26 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,6 @@
 console.log("Up and running!");
 
+/*
 let cardOne = "queen";
 let cardTwo = "queen";
 let cardThree = "king";
@@ -7,3 +8,22 @@ let cardFour = "king";
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+*/
+
+let cards = ["queen","queen","king","king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay[0]);
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}else{
+		alert("Sorry, try again.");
+	}
+}
