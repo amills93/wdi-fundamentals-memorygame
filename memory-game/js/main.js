4819771b09cb3322cc1cfd651274f7ca08c3ff0d
Adam Mills <almills@unomaha.edu>
2020-03-26 17:19:36 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -46,15 +46,25 @@ if(cardsInPlay.length == 2)	{
 }
 }
 
-function flipCard(cardId){
-console.log("User flipped " + cards[cardId].rank);
+function flipCard(){
+//console.log("User flipped " + cards[cardId].rank);
+var cardId = this.getAttribute('data-id');
+this.setAttribute('src', cards[cardId].cardImage);
+
 cardsInPlay.push(cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard(){
+for (let i = 0; i < cards.length; i++) {
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+	}	
+}
 
-
+createBoard();
